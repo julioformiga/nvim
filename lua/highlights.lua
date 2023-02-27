a0287098ecc190426e8e81d0d9d8ced6b3dd522b
Julio Formiga <julio.formiga@gmail.com>
2023-02-27 18:58:09 +0100

    Del: comments

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -1,20 +1,3 @@
--- show breadcrumbs if available
--- local function breadcrumbs()
---     local items = vim.b.coc_nav
---     local t = {''}
---     for k,v in ipairs(items) do
---         setmetatable(v, { __index = function(table, key)
---             return ' '
---         end})
---         t[#t+1] = ' %#' .. (v.highlight or "Normal") .. '#' .. (type(v.label) == 'string' and v.label .. ' ' or '') .. '%#NonText#'.. (v.name or '')
---         if next(items,k) ~= nil then
---             t[#t+1] = '%#StatusLineNC# '
---         end
---     end
---     t[#t+1] = '%#EndOfBuffer#%L  '
---     return table.concat(t)
--- end
---
 local navic = require("nvim-navic")
 navic.setup {
     highlight = false
