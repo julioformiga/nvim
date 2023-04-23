2ef3e36fc64ea94ec18dda5fb9e470cb9baf4d61
Julio Formiga <julio.formiga@gmail.com>
2023-04-23 12:50:38 +0200

    Chg: if neovide, not noice (bug neovide+noice)

--- lua/bootstrap.lua
+++ lua/bootstrap.lua
@@ -31,23 +31,25 @@ require("indent_blankline").setup {
 -- ============ Bug with Neovim https://github.com/neovim/neovim/issues/22344 ===============
 -- ============ Bug with Noice https://github.com/folke/noice.nvim/issues/17 ===============
 -- ============ Bug with Neovide https://github.com/neovide/neovide/issues/1751 ===============
--- require("noice").setup({
---     lsp = {
---         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
---         override = {
---             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
---             ["vim.lsp.util.stylize_markdown"] = true,
---             ["cmp.entry.get_documentation"] = true,
---         },
---     },
---     -- presets = {
---     --     bottom_search = false, -- use a classic bottom cmdline for search
---     --     command_palette = true, -- position the cmdline and popupmenu together
---     --     long_message_to_split = true, -- long messages will be sent to a split
---     --     inc_rename = false,   -- enables an input dialog for inc-rename.nvim
---     --     lsp_doc_border = false, -- add a border to hover docs and signature help
---     -- },
--- })
+if not vim.g.neovide then
+    require("noice").setup({
+        lsp = {
+            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true,
+            },
+        },
+        -- presets = {
+        --     bottom_search = false, -- use a classic bottom cmdline for search
+        --     command_palette = true, -- position the cmdline and popupmenu together
+        --     long_message_to_split = true, -- long messages will be sent to a split
+        --     inc_rename = false,   -- enables an input dialog for inc-rename.nvim
+        --     lsp_doc_border = false, -- add a border to hover docs and signature help
+        -- },
+    })
+end
 
 require("telescope").setup({
     defaults = {
