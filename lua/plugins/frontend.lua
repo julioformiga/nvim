f01c9362fc88c8122dd992c00de76f30950e56ce
Julio Formiga <julio.formiga@gmail.com>
2023-09-17 20:11:43 +0200

    Add: event = InsertEnter in  nvim-autopairs

--- lua/plugins/frontend.lua
+++ lua/plugins/frontend.lua
@@ -33,6 +33,7 @@ return {
     },
     {
         "https://github.com/windwp/nvim-autopairs",
+        event = "InsertEnter",
         config = function()
             require("nvim-autopairs").setup()
         end,
