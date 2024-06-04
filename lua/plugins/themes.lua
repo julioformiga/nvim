5ea86da07057e49f62b4d289e892be6337e23cdd
Julio Formiga <julio.formiga@gmail.com>
2024-06-04 12:41:33 +0200

    Upd: Kanagawa colorscheme options

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -104,13 +104,41 @@ return {
 					},
 				},
 				overrides = function(colors) -- add/modify highlights
-					return {}
+					local theme = colors.theme
+					return {
+						NormalFloat = { bg = "none" },
+						FloatBorder = { bg = "none" },
+						FloatTitle = { bg = "none" },
+
+						-- Save an hlgroup with dark background and dimmed foreground
+						-- so that you can use it where your still want darker windows.
+						-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+						NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+						-- Popular plugins that open floats will link to NormalFloat by default;
+						-- set their background accordingly if you wish to keep them dark and borderless
+						LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+						MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+						Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+						PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+						PmenuSbar = { bg = theme.ui.bg_m1 },
+						PmenuThumb = { bg = theme.ui.bg_p2 },
+
+						TelescopeTitle = { fg = theme.ui.special, bold = true },
+						TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+						TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+						TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+						TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+						TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+						TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+					}
 				end,
 			})
 
-			-- require("kanagawa").load()
+			require("kanagawa").load()
+			vim.cmd("colorscheme kanagawa-wave")
 			-- vim.cmd("colorscheme kanagawa-dragon")
-			-- vim.cmd("colorscheme kanagawa-wave")
 		end,
 	},
 	{
@@ -152,10 +180,11 @@ return {
 				specs = {},
 				groups = {},
 			})
-			require("nightfox").load()
+			-- require("nightfox").load()
 			-- vim.cmd.colorscheme("nightfox")
 			-- vim.cmd.colorscheme("carbonfox")
-			vim.cmd.colorscheme("duskfox")
+			-- vim.cmd.colorscheme("duskfox")
+			-- vim.cmd.colorscheme("terafox")
 		end,
 	},
 	{
