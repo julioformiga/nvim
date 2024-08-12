4ea7af1e1c82fd6064ed8605b8955109a172029d
Julio Formiga <julio.formiga@gmail.com>
2024-08-12 11:01:38 +0200

    Add: plugin Peek, a webview markdown files (deno necessary)

--- lua/plugins/frontend.lua
+++ lua/plugins/frontend.lua
@@ -18,4 +18,14 @@ return {
 			require("nvim-ts-autotag").setup()
 		end,
 	},
+	{
+		"https://github.com/toppair/peek.nvim",
+		event = { "VeryLazy" },
+		build = "deno task --quiet build:fast",
+		config = function()
+			require("peek").setup()
+			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+		end,
+	},
 }
