6199df9f5245cc33e06cb662708436173f429e1a
Julio Formiga <julio.formiga@gmail.com>
2024-09-23 00:44:36 +0200

    add: flash plugin (s, S,... keys)

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -30,10 +30,10 @@ return {
 		},
 	},
 	"https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
-	-- {
-	-- 	"https://github.com/lewis6991/satellite.nvim",
-	-- 	opts = { current_only = true },
-	-- },
+	{
+		"https://github.com/lewis6991/satellite.nvim",
+		opts = { current_only = true },
+	},
 	{
 		"https://github.com/gorbit99/codewindow.nvim",
 		config = function()
@@ -335,5 +335,19 @@ return {
 		name = "Visual MultiCursor",
 		event = "BufEnter",
 		branch = "master",
+	},
+	{
+		"https://github.com/folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+	       -- stylua: ignore
+	       keys = {
+	           { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+	           { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+	           { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+	           { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+	           { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+	       },
 	},
 }
